f0ace02b7b0d33310e9cea65b3b34dea30f44d38
b11005 <instantjoy3@gmail.com>
2015-07-06 17:45:37 +0900

    ./check_arrayはOKにならないが、問題の箇所は目で確認すると存在しない？

--- bisect_sort.py
+++ bisect_sort.py
@@ -1,9 +1,10 @@
 #coding:utf-8
 import bisect
 import binascii
+import sys
 
 
-f=open("test100","rb")
+f=open(sys.argv[1],"rb")
 b=[]
 count=0
 for i in f:
@@ -12,20 +13,12 @@ for i in f:
 	if a==b'':
 		continue
 	a=binascii.hexlify(a)
-	a=int(a,16)
-	#if b==[]: b.append(a)
-	#else: 
+	a=int(a,16) 
 	bisect.insort(b,a)
-	'''for j in range(count-1):
-		if b==[]: b.append(a)
-		elif a>=b[j]:
-			b.append(a)
-		else:
-			b.bisect.insort(0,a)'''
+	
 
-f1=open("result100","w")
-#for i in range(len(b)):
+f1=open(sys.argv[2],"w")
 f1.writelines(str(b))
-	#print(b[i])
+
 f.close()
 f1.close()
