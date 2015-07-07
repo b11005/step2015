c0507a2bb175b0c1affc7b415f0631f34e1f52a4
b11005 <instantjoy3@gmail.com>
2015-07-07 11:13:51 +0900

    1MBまでならソート可能

--- bisect_sort.py
+++ bisect_sort.py
@@ -9,18 +9,19 @@ b=[]
 count=0
 for i in f:
 	count+=1
-	a=f.read(4)
+	a=f.read(2)
 	if a==b'':
 		continue
-	#a=binascii.hexlify(a)
-	#a=int(a,16) 
+	a=binascii.hexlify(a)
+	a=int(a,16) 
+	count+=1
 	bisect.insort(b,a)
 	
-
-#f1=open(sys.argv[2],"w")
-#f1.writelines(str(b))
-f1=open(sys.argv[2],"wb")
-f1.writelines(b)
+print (count)
+f1=open(sys.argv[2],"w")
+f1.writelines(str(b))
+#f1=open(sys.argv[2],"wb")
+#f1.writelines(b)
 
 f.close()
 f1.close()
